cb56368fa9c2995fe03ee2cf04b961e839552597
esatiyev <108625030+esatiyev@users.noreply.github.com>
2025-04-03 22:06:48 +0900

    Last commit

--- include/saha_robot_cpp/visibility_control.h
+++ include/saha_robot_cpp/visibility_control.h
@@ -8,7 +8,6 @@ extern "C"
 
 // This logic was borrowed (then namespaced) from the examples on the gcc wiki:
 //     https://gcc.gnu.org/wiki/Visibility
-
 #if defined _WIN32 || defined __CYGWIN__
   #ifdef __GNUC__
     #define ACTION_TUTORIALS_CPP_EXPORT __attribute__ ((dllexport))
